5d7684116f3282f755ae7567676514043cdfad43
dakshitha <dakshitha@f2ca67fc-6536-4822-b264-2774915f19b4>
2011-06-08 11:27:09 +0000

    Changed pom-gen mojo.

--- src/main/java/org/wso2/maven/bpel/artifact/BPELPOMGenMojo.java
+++ src/main/java/org/wso2/maven/bpel/artifact/BPELPOMGenMojo.java
@@ -18,25 +18,35 @@ package org.wso2.maven.bpel.artifact;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.stream.FactoryConfigurationError;
+
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.model.Plugin;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.wso2.maven.capp.bundleartifact.AbstractBundlePOMGenMojo;
 import org.wso2.maven.capp.model.Artifact;
+import org.wso2.maven.capp.model.BundlesDataInfo;
 import org.wso2.maven.capp.mojo.AbstractPOMGenMojo;
 import org.wso2.maven.capp.utils.CAppMavenUtils;
 
 /**
- * This is the Maven Mojo used for generating a pom for a BPEL artifact
- * from the old CApp project structure
+ * This is the Maven Mojo used for generating a pom for a BPEL artifact from the
+ * old CApp project structure
  *
  * @goal pom-gen
  *
  */
-public class BPELPOMGenMojo extends AbstractPOMGenMojo {
-
+public class BPELPOMGenMojo extends AbstractBundlePOMGenMojo {
 	/**
 	 * @parameter default-value="${project}"
 	 */
@@ -78,30 +88,110 @@ public class BPELPOMGenMojo extends AbstractPOMGenMojo {
 	public String groupId;
 
 	/**
-	 * Comma separated list of "artifact_type=extension" to be used when creating dependencies for other capp artifacts
+	 * Comma separated list of "artifact_type=extension" to be used when
+	 * creating dependencies for other capp artifacts
 	 *
 	 * @parameter
 	 */
 	public String typeList;
 
+	/**
+	 * A list of projects in eclipse workspace which can be referred using maven
+	 * groupid, artifactid, version
+	 *
+	 * @parameter
+	 */
+	private List<String> projects;
 
-	private static final String ARTIFACT_TYPE="bpel/workflow";
+	/**
+	 * @component
+	 */
+	public ArtifactFactory artifactFactory;
 
+	/**
+	 * @component
+	 */
+	public ArtifactResolver resolver;
 
-	protected void copyResources(MavenProject project, File projectLocation, Artifact artifact)throws IOException {
-		File bpelArtifact = artifact.getFile();
-		FileUtils.copyFile(bpelArtifact, new File(projectLocation, bpelArtifact.getName()));
-	}
-	protected void addPlugins(MavenProject artifactMavenProject, Artifact artifact) {
-		Plugin plugin = CAppMavenUtils.createPluginEntry(artifactMavenProject,"org.wso2.maven","maven-bpel-plugin","1.0.0",true);
-		Xpp3Dom configuration = (Xpp3Dom)plugin.getConfiguration();
-		//add configuration
-		Xpp3Dom aritfact = CAppMavenUtils.createConfigurationNode(configuration,"artifact");
-		aritfact.setValue(artifact.getFile().getName());
-	}
+	/**
+	 * @parameter default-value="${localRepository}"
+	 */
+	public ArtifactRepository localRepository;
+
+	/**
+	 * @parameter default-value="${project.remoteArtifactRepositories}"
+	 */
+	public List<?> remoteRepositories;
+
+	private static final String ARTIFACT_TYPE = "bpel/workflow";
+
+	private BundlesDataInfo bundlesDataInfo;
+
+	private List<String> artifactProjects;
 
 	protected String getArtifactType() {
 		return ARTIFACT_TYPE;
+	}
+
+	protected List<String> getProjectMapStrings() {
+		return projects;
+	}
+
+	public ArtifactFactory getArtifactFactory() {
+		return artifactFactory;
+	}
+
+	public ArtifactResolver getResolver() {
+		return resolver;
+	}
+
+	public ArtifactRepository getLocalRepository() {
+		return localRepository;
+	}
+
+	public List<?> getRemoteRepositories() {
+		return remoteRepositories;
+	}
+
+	protected String getBundleActivatorClass(Artifact artifact) {
+		return null;
+	}
+
+	protected void addPlugins(MavenProject artifactMavenProject, Artifact artifact) {
+	}
+
+	protected BundlesDataInfo getBundlesDataInfo(File targetProjectLocation, Artifact artifact)
+			throws FactoryConfigurationError {
+		if (bundlesDataInfo == null) {
+			try {
+				bundlesDataInfo = new BundlesDataInfo();
+				bundlesDataInfo.setProjects(getProjectMappings());
+				List<String> artifactProjects = getArtifactProjects();
+				for (String artifactProject : artifactProjects) {
+					String[] artifactProjectData = artifactProject.split(":");
+					if (artifactProjectData.length == 2 && artifactProjectData[0].equals(artifact.getName())) {
+						String[] projectNames = artifactProjectData[1].split(",");
+						for (String projectName : projectNames) {
+							bundlesDataInfo.addProjectToList(projectName, null);
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return bundlesDataInfo;
+	}
+
+	public List<String> getArtifactProjects() {
+		if (artifactProjects == null) {
+			artifactProjects = new ArrayList<String>();
+		}
+		return artifactProjects;
+	}
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// Nothing to do. No need to generate a pom.
 	}
 
 }
